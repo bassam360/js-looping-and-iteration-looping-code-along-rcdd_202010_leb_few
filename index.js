4cb9294e7f14021cf01209f57f6e83b57d1fb737
bassam edelbi <bassamedelbi360@gmail.com>
2020-12-28 10:47:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+/*writeCards(  );
+
+Function writeCards([ 'Lisa', 'Kaitlin', 'Jan' ],'surprise'){
+  for(i=0;i<x.length;i++){
+     console.log(`Wrapped , ${gifts[i]} , and added a bow!`);
+  }
+  
+}
+*/
+
+countdown(10)
+
+Function countdown(){
+   while(x<0){
+     
+     console.log(x);
+     x--;
+   }
+  
+  
+}
+
+
